6e4bf9f054e0f20ad16bd0f9070f940d503ce16c
Lasha-Muza <lashamuzashvili@redberry.ge>
2022-08-28 13:00:28 +0400

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Movie Quotes</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <div class="bg-red-500 w-40 h-20 text-center">
+        <p>{{ __('texts.hello_world') }}</p>
+    </div>
+</body>
+</html>
