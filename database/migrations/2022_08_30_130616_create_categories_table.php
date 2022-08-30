cb5e9cad8d0f453faa14dc9c91c0676c55afe0e0
Lasha-Muza <lashamuzashvili@redberry.ge>
2022-08-30 18:05:30 +0400

    fix: changed naming

--- database/migrations/2022_08_30_130616_create_categories_table.php
+++ database/migrations/2022_08_30_130616_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration {
 	{
 		Schema::create('categories', function (Blueprint $table) {
 			$table->id();
-			$table->string('genre_id')->unique();
+			$table->string('genre')->unique();
 			$table->timestamps();
 		});
 	}
