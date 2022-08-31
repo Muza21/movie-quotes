703f8cab5c115a1a59325588f7bac35d85810c51
Lasha-Muza <lashamuzashvili@redberry.ge>
2022-08-31 15:59:14 +0400

    refactor: testing refactoring

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -2,7 +2,6 @@
         <div class="w-full max-w-4xl mx-auto my-20 bg-white shadow-lg rounded-xl border border-gray-200">
             <header class="px-9 py-4 border-b border-gray-100">
                 <select name="category" id="category">
-                    {{-- Here are options that are from database --}}
                     @foreach (\App\Models\Category::all() as $category)
                         <option
                             value="{{ $category->genre }}"
@@ -18,7 +17,6 @@
                             <tr>
                                 <td class="px-6 py-4 whitespace-nowrap text-left text-sm font-medium">
                                     <div class="font-medium text-gray-800 underline">
-                                        {{-- this will take user to the Movie's quotes --}}
                                         <a href="/posts" class="hover:text-green-500"> 
                                             {{ $post->title }}
                                         </a>
@@ -26,13 +24,11 @@
                                 </td>
                                 <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                                     <div class="text-left text-green-500">
-                                        {{-- This will take user to create new quote of this current movie --}}
                                         <a href="/admin/posts/edit" class="hover:text-green-600">Add Quote</a>
                                     </div>
                                 </td>
                                 <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                                     <div class="text-left font-medium text-orange-400">
-                                        {{-- This will take user to remove or add new quotes or edit current quotes page --}}
                                         <a href="/admin/posts/edit" class="hover:text-orange-600">Edit</a>
                                     </div>
                                 </td>
